97cd58ba5700e794cf843782da270c3ceb78c4d8
Vinayak Shukla <98159594+V-inaya-K@users.noreply.github.com>
2022-09-27 17:05:06 +0530

    first

---
+++ click.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Its a Blog website where you can dive deep into ocean of knowledge.";
+  } //function for first faq
+function myFunction1() {
+    document.getElementById("demo2").innerHTML = "A blog is a discussion or informational website published on the World Wide Web consisting of discrete, often informal posts.";
+  } //function for second faq
+
+function myFunction2() {
+    document.getElementById("demo3").innerHTML = "LinkedIn is one such blogging website on which you can get hired.";
+  } //function for third faq
+function myFunction3() {
+    document.getElementById("demo4").innerHTML = "1)Press Create Button.<br/> 2)Add subject and heading.<br/>  3)Submit the blog and all set you are ready!";
+  } //function for fourth faq
